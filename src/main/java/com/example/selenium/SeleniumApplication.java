8a5bf179a0f3cc24d03cec7d74aa3421541b7bc5
Martin Sacco <martin.sacco@softvision.com>
2020-11-06 12:18:19 -0300

    First test working

--- src/main/java/com/example/selenium/SeleniumApplication.java
+++ src/main/java/com/example/selenium/SeleniumApplication.java
@@ -1,13 +1,27 @@
 package com.example.selenium;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-@SpringBootApplication
 public class SeleniumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeleniumApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+
+        WebDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("http://www.facebook.com");
+        chromeDriver.findElement(By.xpath("//input[@id='email']")).sendKeys("usuario");
+        chromeDriver.findElement(By.xpath("//input[@type='password']")).sendKeys("password");
+        chromeDriver.findElement(By.id("u_0_b")).click();
+
+
+
+    }
+
 
 }
+
+
